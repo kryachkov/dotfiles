6d850d46dc79f32403ca3c6fb91d0ab601eaeb0b
André Falk <andre.falk@tutanota.com>
2024-07-31 16:48:11 +0200

    Add some good stuff to nvim config

--- init.lua
+++ init.lua
@@ -71,6 +71,7 @@ require("lazy").setup({
   'tpope/vim-fugitive',
   'tpope/vim-surround',
   'justinmk/vim-sneak',
+  'nvim-treesitter/nvim-treesitter',
 }, opts)
 
 -- <Space> is <Leader>
@@ -177,16 +178,23 @@ local cmp_format = require('lsp-zero').cmp_format()
 require('luasnip.loaders.from_vscode').lazy_load()
 
 cmp.setup({
-  sources = {
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+
+  sources = cmp.config.sources({
     { name = "buffer" },
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
-  },
+  }),
+
   mapping = cmp.mapping.preset.insert({
     ['<Tab>'] = cmp_action.luasnip_jump_forward(),
     ['<S-Tab>'] = cmp_action.luasnip_jump_backward(),
   }),
-  --- (Optional) Show source name in completion menu
+  -- (Optional) Show source name in completion menu
   formatting = cmp_format,
 })
 
