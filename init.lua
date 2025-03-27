1a8604584439c6ad7bb1be1de11c00e76c09d391
André Falk <andre.falk@tutanota.com>
2025-03-27 08:02:28 +0100

    nvim config updates
    Adds navigation to gitsigns
    Changes colorscheme

--- init.lua
+++ init.lua
@@ -64,7 +64,34 @@ require("lazy").setup({
   {
     'lewis6991/gitsigns.nvim',
     config = function()
-      require('gitsigns').setup()
+      require('gitsigns').setup{
+        on_attach = function(bufnr)
+          local gitsigns = require('gitsigns')
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          -- Navigation
+          map('n', ']c', function()
+            if vim.wo.diff then
+              vim.cmd.normal({']c', bang = true})
+            else
+              gitsigns.nav_hunk('next')
+            end
+          end)
+
+          map('n', '[c', function()
+            if vim.wo.diff then
+              vim.cmd.normal({'[c', bang = true})
+            else
+              gitsigns.nav_hunk('prev')
+            end
+          end)
+        end
+      }
     end
   },
 
@@ -169,7 +196,7 @@ imap('<F7>', '<CMD>LspStop<CR>')
 nmap('<F8>', '<CMD>LspStart<CR>')
 imap('<F8>', '<CMD>LspStart<CR>')
 
-vim.cmd('colorscheme habamax')
+vim.cmd('colorscheme sorbet')
 vim.cmd('highlight Normal guibg=none')
 -- vim.cmd('colorscheme darkblue')
 
